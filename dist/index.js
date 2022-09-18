b522a618cb09d53bb093354e1ff8f2bfb216f9f8
arijit <arijitmaiti@technoexponent.com>
2022-09-18 21:56:54 +0530

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Demo", {
+  enumerable: true,
+  get: function get() {
+    return _Demo.default;
+  }
+});
+
+var _Demo = _interopRequireDefault(require("./components/Demo"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
